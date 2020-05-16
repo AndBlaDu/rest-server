533ccff74c7f451a3590c715107e844ba8034416
Andres <andresblanco03.ab@gmail.com>
2020-05-15 21:18:58 -0600

    rest server update

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,17 @@
 //PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//DB
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev')
+    urlDB = 'mongodb://localhost:27017/cafe';
+else
+    urlDB = 'mongodb+srv://andbladu:RxQzmDhbQoOowXq8@cluster0-orqdi.mongodb.net/cafe';
+
+process.env.URLDB = urlDB;
